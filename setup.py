517c7f16866a25ad9f9f297d298fc4b42f56c3b5
Gowthaman <egowtham@LAMU02W4B08HTD6.uhc.com>
2020-09-30 15:09:51 -0500

    update repo.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A wrapper for kafka-python used in knative",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/egowtham/knativekafka",
+    url="https://github.com/ezhil-g/knativekafka",
     packages=['knativekafka'],
     python_requires='>=3.7',
     install_requires=['kafka-python'],
